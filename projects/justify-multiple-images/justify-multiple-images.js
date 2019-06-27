b2c7de09908a7e5a0ec36ef0828e649741ee8be1
Venkata Ramana <pvrreddy155@gmail.com>
2019-06-27 15:49:54 +0530

    Update justify-multiple-images.js

--- projects/justify-multiple-images/justify-multiple-images.js
+++ projects/justify-multiple-images/justify-multiple-images.js
@@ -9,8 +9,8 @@
 			var par_width = $(this).width();
 			var child_height = $(this).find('img').height();
 			var child_width = $(this).find('img').width();
-			child_height = (child_height > par_height) ? par_height:child_height;
-			child_width = (child_width > par_width) ? par_width:child_width;
+			//child_height = (child_height > par_height) ? par_height:child_height;
+			//child_width = (child_width > par_width) ? par_width:child_width;
 			var isOverflown = $(this)[0].scrollHeight > $(this)[0].clientHeight || $(this)[0].scrollWidth > $(this)[0].clientWidth;
 
 			while(isOverflown){
